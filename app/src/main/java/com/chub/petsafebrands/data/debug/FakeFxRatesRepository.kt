12d70a2e2cdad28833155733c5d826496af1cce1
anatoliy <anatoliy.chub.dev@gmail.com>
2024-09-07 17:28:26 +0200

    Lint warnings fixed.

--- app/src/main/java/com/chub/petsafebrands/data/debug/FakeFxRatesRepository.kt
+++ app/src/main/java/com/chub/petsafebrands/data/debug/FakeFxRatesRepository.kt
@@ -6,7 +6,7 @@ import com.chub.petsafebrands.data.retrofit.Result
 import com.google.gson.Gson
 import javax.inject.Inject
 
-class FakeFxRatesRepository @Inject constructor(private val apiService: MockApiService, val gson: Gson) :
+class FakeFxRatesRepository @Inject constructor(private val apiService: MockApiService) :
     FxRatesRepository {
     override suspend fun getRates(base: String): Result<ExchangeRatesResponse> {
         return apiService.getRates(base)
